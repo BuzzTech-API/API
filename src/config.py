23fab991323340ad3958d6473ecc16ce73a5bf9c
Isaque Elis da Silva <90930885+KhovetS2@users.noreply.github.com>
2022-10-29 22:05:38 -0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Apolo9654#@localhost:5523/api'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost:3306/api'
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
 SECRET_KEY = "SECRET"
 SQLALCHEMY_TRACK_MODIFICATIONS = True

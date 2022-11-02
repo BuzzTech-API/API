60854fd23e08b0c85dbabb86f28558f78be5a0ae
Giuliano Marcus Bianco <giuliano.mbianco@gmail.com>
2022-11-01 23:58:05 -0300

    feature: banco de dados na vm aws

--- src/config.py
+++ src/config.py
@@ -1,11 +1,7 @@
 
 DEBUG = True
 
-<<<<<<< HEAD
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Apolo9654#@localhost:5523/api'
-=======
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:/Cassiopeia69@localhost:3306/api'
->>>>>>> main
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://fatec:fatec@35.174.216.24:3306/api'
 #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://b70864addbd6db:97156f7a@us-cdbr-east-06.cleardb.net/heroku_ebe0c393af7b9fc'
 SECRET_KEY = "SECRET"
 SQLALCHEMY_TRACK_MODIFICATIONS = True

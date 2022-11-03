c10e9a370631252e1fefe8de3d054eab0a7d1640
Vitor Spricigo Lima <112436453+lima2206@users.noreply.github.com>
2022-11-03 20:43:05 -0300

    Comentário de como mudar o banco para local host.

--- src/config.py
+++ src/config.py
@@ -1,7 +1,8 @@
 
 DEBUG = True
 
-
+# Para utilização do banco local. Trocar para:
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:'senhalocal'@localhost:3306/api'
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://fatec:fatec@35.174.216.24:3306/api'
 
 SECRET_KEY = "SECRET"

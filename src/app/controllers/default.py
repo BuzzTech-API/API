54bee44c9ff2a5c059eb0293c16b2f79d60748cd
Cláudia CBS <112169639+ClaudiaCBS@users.noreply.github.com>
2022-11-05 17:23:37 -0300

    Função de alteração de especificação para a RAM, processador e SO

--- src/app/controllers/default.py
+++ src/app/controllers/default.py
@@ -18,14 +18,21 @@ def homepage():
     return render_template("home.html")
 
 
-@app.route("/<lab>/<comp>/acao", methods=["POST", "GET"])
-def acao(lab,comp):
+
+# função de alteração de especificação para a RAM, processador e SO
+@app.route("/<lab>/<comp>/especificacao", methods=["POST", "GET"])
+def especificacao(lab,comp):
     computador=Object.query.filter_by(id=int(comp)).first()
+    ram= request.form['ram']
+    computador.Object_comp_RAM=ram
+    sistema_operacional= request.form['sistema_operacional']
+    computador.Object_comp_operational_system=sistema_operacional
     processador= request.form['processador']
     computador.Object_comp_processor=processador
     db.session.add(computador)
     db.session.commit()
     return redirect(f'/{lab}/{comp}/seleção_problemas')
+
 
 @app.route("/obrigado")
 def obrigado():

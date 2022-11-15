95c913cd58ced9685ce21bd926de335dddca06bb
Joice-Araujo <112172335+Joice-Araujo@users.noreply.github.com>
2022-11-15 16:35:14 -0300

    Atualização dos menus, ainda falta colocar uma rota para a pag contatos

--- src/config.py
+++ src/config.py
@@ -3,7 +3,8 @@ DEBUG = True
 
 
 # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://fatec:fatec@35.174.216.24:3306/api'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@localhost:3306/api'
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@localhost:3306/api'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Pitoco22@localhost:3306/api'
 
 SECRET_KEY = "SECRET"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
